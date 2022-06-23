8373cdcde4b2d83a38449182149f1c230d5a4dad
minawilly <aminahwilliamsade@gmail.com>
2022-06-23 01:41:23 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var mname;
+mname=prompt("Enter Your name");
+
+var height;
+height=prompt("What is your height");
+
+var country;
+country=prompt("What country are you from?");
+
+alert("Hello! I'm" + " " + mname + ", " + "I'm" + " " + height + " " + "tall" + " " + "and" + " " + "I'm from" + " " + country + ".");
